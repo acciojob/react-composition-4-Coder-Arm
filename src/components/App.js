fd0d3d430fba0cd9d6dbb65a89e8217594732ca5
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-08-17 10:52:27 +0530

    push

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name , setName] = useState("");
+  const [email , setEmail] = useState("");
+  const [pass , setPass] = useState("");
+  const [user,setUser] = useState("");
+   console.log(user);
+  
+  function updateUser(e){
+    e.preventDefault();
+    setUser({'name' : name , 'email' : email , 'password' : pass})
+    setName("")
+    setEmail("")
+    setPass("")
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={updateUser}>
+        <label htmlFor="name">Name <input type="text" value={name} id="name" onChange={(e) => setName(e.target.value)}/></label>
+        <label htmlFor="email">Email <input type="text" value={email}  id="email" onChange={(e) => setEmail(e.target.value)}/></label>
+        <label htmlFor="pass">Password <input type="text" value={pass}  id="pass" onChange={(e) => setPass(e.target.value)}/></label>
+        <button>onSubmit</button>
+      </form>
     </div>
   )
 }
